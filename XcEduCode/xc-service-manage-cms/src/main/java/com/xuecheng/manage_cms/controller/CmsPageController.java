772f77076a4e83c54eff8c5f517f2cff88a37bd1
sinnaeanlin <1043485232@qq.com>
2021-04-06 17:01:37 +0800

    20210406

---
+++ XcEduCode/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsPageController.java
@@ -0,0 +1,36 @@
+package com.xuecheng.manage_cms.controller;
+
+import com.xuecheng.api.cms.CmsPageControllerApi;
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.QueryResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cms/page")
+public class CmsPageController implements CmsPageControllerApi{
+    @Override
+    @GetMapping("/list/{page}/{size}")
+    public QueryResponseResult findList(@PathVariable("page") int page,@PathVariable("size") int size, QueryPageRequest queryPageRequest) {
+        /*
+        QueryResult<CmsPage> queryResult = new QueryResult<>();
+        List<CmsPage> list = new ArrayList<>();
+        CmsPage cmsPage = new CmsPage();
+        cmsPage.setPageName("测试页面");
+        list.add(cmsPage);
+        queryResult.setList(list);
+        queryResult.setTotal(1);
+        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS,queryResult);
+        return queryResponseResult;
+        */
+        return null;
+    }
+}
